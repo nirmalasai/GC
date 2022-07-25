bf6b4d9810432408ee6899e1306616cd47fc9a8c
Nirmala <sakhinetipalli.nirmala@gmail.com>
2022-07-25 12:39:18 +0530

    commit

---
+++ src/test/java/com/first/git/MyFirstGitCmd.java
@@ -0,0 +1,11 @@
+package com.first.git;
+
+public class MyFirstGitCmd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My First command");
+
+	}
+
+}
